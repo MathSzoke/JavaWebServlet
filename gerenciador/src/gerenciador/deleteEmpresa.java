d57971c539298223df0740dbd0423ae13d1acb86
Matheus Henrique Szoke La Motta <Matheusszoke@gmail.com>
2021-06-18 11:29:07 -0300

    commit

---
+++ gerenciador/src/gerenciador/deleteEmpresa.java
@@ -0,0 +1,29 @@
+package gerenciador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class deleteEmpresa
+ */
+@WebServlet("/deleteEmpresa")
+public class deleteEmpresa extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String paramID = request.getParameter("id");
+		Integer id = Integer.valueOf(paramID);
+		
+		System.out.println(id);
+		
+		Banco banco = new Banco();
+		banco.removeEmpresa(id);
+		
+		response.sendRedirect("listaEmpresas");
+	}
+}
